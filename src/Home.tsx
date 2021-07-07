61d51c1cdb61f9f7e2db08773965ffb7020fe913
sudo180272 <sudo180272@protonmail.ch>
2021-07-08 01:31:12 +0900

    clean boilerplate

---
+++ src/Home.tsx
@@ -0,0 +1,22 @@
+import { Component } from 'react'
+import './Home.css'
+import './master.css'
+
+class Home extends Component {
+  render() {
+    return (
+      <div
+        className="container"
+      >
+        <div
+          className="title"
+        >
+          BHSJP
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Home
+
